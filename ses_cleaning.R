a4552f63a91c00a9eaf6efbdf6807760a688d8e5
Rebecca Witinok-Huber <rebecca.witinok-huber@colostate.edu>
2020-12-07 11:02:22 -0700

    mapping ses vars

--- ses_cleaning.R
+++ ses_cleaning.R
@@ -9,7 +9,6 @@ library(maps)
 library(ggmap)
 
 #Import our datasets----
-#policy <- read.csv("raw_data/state_policy_updates_20201122_0721.csv")
 #covid_cases <- read.csv("raw_data/covid_outbreak.csv") # no geometry here
 #covid_deaths <- read.csv("raw_data/covid_deaths.csv") #city avail here
 
@@ -22,11 +21,6 @@ ses <- read.csv("raw_data/Income_Poverty__Census_Tracts_.csv")
 compare_fips <- ses %>%
   select(FIPS, Tract_FIPS) %>%
   mutate(compare = ifelse(as.numeric(FIPS) == as.numeric(Tract_FIPS), TRUE, FALSE))
-
-#subset Larimer county from other CO counties?
-larimer_ses <- ses %>%
-  select(Tract_FIPS:Poverty_Per_Capita_Income) %>%
-  filter(County == "LARIMER") 
 
 #example from Bree
 larimer_tracts <- tracts(state = 08, county =069, 
@@ -41,28 +35,28 @@ larimer_info <- full_join(larimer_income, larimer_tracts,
                           by = list(x = "FIPS", y = "GEOID"))
 
 
+library(viridis)
+library(scales)
+#plot larimer cities 
+
+larimer_short <- larimer_info %>%
+  select(Population_Total:Poverty_Per_Capita_Income, TRACTCE)
+
+larimer_tracts %>% left_join(.,larimer_short, by = "TRACTCE") %>%
+  ggplot() +
+  geom_sf(aes(fill = Population_Total)) +
+  geom_point(data = larimer_gps %>%
+               filter(City!="Cedaredge"), aes(x=Lon, y=Lat)) +
+  geom_text(data = larimer_gps %>%
+              filter(City!="Cedaredge"), 
+            aes(x=Lon, y=Lat, 
+                label = City), color = "white",
+            show.legend=NA, 
+            nudge_x = .1, nudge_y = .1,
+            angle = 25) +
+  ggtitle("Population Totals for Larimer County")
 
 
-
-
-
-# example using tigris census to create maps with fips codes----
-co_counties <- counties(state= "CO", cb = TRUE, class = "sf")
-class(co_counties)
-
+  
 ggplot() +
-  geom_sf(data = co_counties, aes(fill = ALAND)) +
-  ggtitle("Land acres of Colorado conties") +
-  theme_dark() +
-  scale_fill_viridis("Land area")
-
-larimer <- co_counties %>%
-  filter(NAME == "Larimer")
-
-ggplot() +
-  geom_sf(data=co_counties, color = "lightgray") +
-  geom_sf(data=larimer, fill = "darkcyan") +
-  geom_sf_text(data=larimer, aes(label = NAME), color = "white") +
-  theme_dark() + labs(x= "", y="")
-
-
+  geom_sf(data = larimer_tracts) 
